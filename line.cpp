78553734b8f5dc6f35d1db6505865eb269413845
BrandonJudd <brandon.judd@outlook.com>
2016-05-05 11:33:04 +1200

    Update line.cpp
    Notes added for what I what the code to do.

--- line.cpp
+++ line.cpp
@@ -21,13 +21,31 @@ extern "C" int save_picture(char filename[5]);
 
 extern "C" int set_motor(int motor,int speed);
 
-extern "C" int read_analog(int ch_adc);
-extern "C" int Sleep(int sec, int usec);
-extern "C" int select_IO(int chan, int direct);
-extern "C" int write_digital(int chan,char level);
-extern "C" int read_digital(int chan);
-extern "C" int set_PWM(int chan, int value);
-
 extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
+
+int line() {
+  take_picture();      // take camera shot
+       get_pixel(100, 55 ,255,0,0);
+       // display picture
+       update_screen();
+}
+//What I want it to do:
+//The robot will know its in the correct direction if there is a certain amount of white pixels in center of the image.
+//If there isn't a certain amount it will take smaller rectangle of the sampled area and count the white pixels again.
+//Depending on these values it will know where there are less white pixels so it will know what way it needs to turn
+//It will keep turning until there is a certain amount of white pixels in the frame.
+
+//Read the image and read the amount of white/semi white pixels in the center of the image and add each white pixel to variable count.
+//Count will have the total amount of white/semi white pixels.
+
+//If count is above a certain amount the robot will keep moving forward.
+//If there is not it will take count the amount of white pixels in smaller portions of the image (Left and right of center)
+//If the left side has less pixels then the robot will know to turn left, and it will keep moving left until there is a certain amount of pixles in the frame.
+//Same for the right side.
+
+//Will return the value to the moving class whether to turn left, right or keep moving forward. 0 for left 1 for right.
+
+//camera size 160x160??????  25600 pixles???
+
