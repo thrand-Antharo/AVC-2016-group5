a01661ce130d73fa9d7a532cb6ee52721bbfaa51
auckracait <auckramc@gmail.com>
2016-05-12 11:39:38 +1200

    update

--- testMain.cpp
+++ testMain.cpp
@@ -32,6 +32,19 @@ extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
 
+int  main (){
+  
+  init(1);
+    connect_to_server("130.195.6.196", 1024); // port number ???? pos not right 
+    send_to_server("Please");
+    
+    char message[24];
+    recieve_from_server(message);
+    printf("%s", message);
+    
+    return 0;} 
+
+
 
 int normalSpeed = 135;
 int v_left = normalSpeed;
