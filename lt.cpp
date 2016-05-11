a5980bc515cc47c8c5fb14e7846425e77e0ef187
auckracait <auckramc@gmail.com>
2016-05-12 11:20:42 +1200

    netowrk update

--- lt.cpp
+++ lt.cpp
@@ -34,6 +34,21 @@ extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
 
+int main (){
+    
+    init(1);
+    connect_to_server("130.195.6.196", 1024); // port number ???? pos not right 
+    sen_to_server("Hello server");
+    
+    char message[24];
+    recieve_from_server(message);
+    printf("%s", message);
+    
+    return 0;} 
+
+    
+
+
 
 int v_left = 0;
 int v_right = 0;
