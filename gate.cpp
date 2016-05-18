19239d5c0337c63d0f8501980cbf5aba64d764d9
BrandonJudd <brandon.judd@outlook.com>
2016-05-19 11:30:50 +1200

    Update gate.cpp

--- gate.cpp
+++ gate.cpp
@@ -5,25 +5,21 @@
 extern "C" int init_hardware();
 extern "C" int init(int d_lev);
 
+extern "C" int connect_to_server( char server_addr[15],int port);         
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
 
-
-
-
-
-void open_gate()        //to open gate - call/class thingy
-{
-char ip[15] = "130.195.6.196";      //address
-int port = 1024;  //port
-char request [24] = "Please";     //password/message 
-char passord [24];  //message
-connect_to_server(ip, port); //char server_addr[15], int port
-
-send_to_server(request); //to open - char message[24]
-
-
-recieve_from_server(password);     //char message[24]
-
-send_to_server(password);
+void open_gate() {       //to open gate - call/class thingy
+//initialising variables
+  char ip[15] = "130.195.6.196";      //address
+  int port = 1024;  //port
+  char request [24] = "Please";     //password/message 
+  char passord [24];  //message
+  connect_to_server(ip, port); //char server_addr[15], int port
+  send_to_server(request); //to open - char message[24]
+  recieve_from_server(password);     //char message[24]
+  send_to_server(password);
+  
 }
 
 
@@ -35,11 +31,9 @@ send_to_server(password);
 //however it is long and partially copied from the example so not 100% if its a go
 
 
-extern "C" int connect_to_server( char server_addr[15],int port);         
-extern "C" int send_to_server(char message[24]);
-extern "C" int receive_from_server(char message[24]);
 
-int  doGate (){
+
+/*int  doGate (){
   
 init(1);
  connect_to_server("130.195.6.196", 1024); // port number ???? pos not right 
@@ -50,4 +44,4 @@ init(1);
   printf("%s", message);
     
   return 0;} 
-doGate()
+doGate()*/
