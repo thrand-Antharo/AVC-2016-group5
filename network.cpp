e3a60b145dbdbf3984b42a8bda490b90ca8e23a8
zhoupai <1349784129@qq.com>
2016-05-20 11:07:47 +1200

    Update network.cpp

--- network.cpp
+++ network.cpp
@@ -1,26 +1,123 @@
 
-# include <studio.h>
-# include <time.h>
+#include <stdio.h>
 
+//#include <pthread.h>
+#include <time.h>
+#include <string.h>
+
+// sudo gcc -Wall
+extern "C" int init_hardware();
 extern "C" int init(int d_lev);
-extern "C" int connect_to_server(char server_addr[?], int port)        // address ?? 
-extern "C" int send_to_server(char message[?]);                       //message ?? 
-extern "C" int recieve_fro_server(char message[?]);                   //message ?? 
 
-int main (){
+extern "C" int take_picture();
+extern "C" char get_pixel(int row, int col, int color);
+extern "C" void set_pixel(int col, int row, char red,char green,char blue);
 
+extern "C" int open_screen_stream();
+extern "C" int close_screen_stream();
+extern "C" int update_screen();
+//extern "C" void GetLine(int row,int threshold);
+extern "C" int display_picture(int delay_sec,int delay_usec);
+extern "C" int save_picture(char filename[5]);
+
+extern "C" int set_motor(int motor,int speed);
+
+extern "C" int read_analog(int ch_adc);
+extern "C" int Sleep(int sec, int usec);
+extern "C" int select_IO(int chan, int direct);
+extern "C" int write_digital(int chan,char level);
+extern "C" int read_digital(int chan);
+extern "C" int set_PWM(int chan, int value);
+
+extern "C" int connect_to_server( char server_addr[15],int port);         
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+
+//int  doGate (){
+  
+//  init(1);
+//    connect_to_server("130.195.6.196", 1024); // port number ???? pos not right 
+//    send_to_server("Please");
+    
+//    char message[24];
+//    recieve_from_server(message);
+//    printf("%s", message);
+    
+//    return 0;} 
+
+int line(int row) {
+  int sum = 0;
+  int kp = 0.3;//example value, testing needed
+  int w, s;
+  int proportional_signal;
+  take_picture();      // take camera shot
+  for(int num=0; num < 320; num++){
+    w=get_pixel(num, row, 3);
+    if(w>127){s=1;}//if it's closer to white
+    else{s=0;}
+    sum = sum + (num-160)*s;
+  }
+  update_screen();
+  proportional_signal = sum*kp;
+  return proportional_signal;
 }
 
+int motorControl(float error_signal)
+{
+  printf("Average error signal: %f\n",error_signal);
+  int SPEED = 30;
+    float change = (error_signal/SPEED)/10;
+    //if too far left
+    if(error_signal < 0){
+      set_motor(1,(SPEED/2));//right motor
+      set_motor(2,SPEED);//left motor
+      printf("Too far left!\n");
+      printf("Left motor: %d Right motor %d\n",SPEED,(SPEED/2))
+    }
+    //if too far right
+    else if(error_signal > 0){
+      set_motor(1,SPEED);
+      set_motor(2,(SPEED/2));
+      printf("Too far right!\n");
+      printf("Left motor: %d Right motor %d\n", (SPEED/2), SPEED)
+    }
+    //if centered
+    else{
+      set_motor(1,SPEED);
+      set_motor(2,SPEED);
+      printf("Going straight\n");
+    }
+    return 0;
+}
+
+int main()
+{
+    int i;
+    init(0);
+    // connect camera to the screen
+    open_screen_stream();
+    // set all didgital outputs to +5V
+    for (i = 0; i < 8; i++)
+    {
+      // set all digital channels as outputs
+      select_IO(i,0);
+      write_digital(i,1);
+    }
+    while(1)
+    {
+      int s1 = line (110);
+      int s2 = line (120);
+      int s3 = line (130);
+      float AvgSignal = (s1+s2+s3)/3;
+      motorControl(AvgSignal);
+     }
+
+   // terminate hardware
+    close_screen_stream();
+    set_motor(1,0);
+    set_motor(2,0);
+
+    return 0;
 
 
-
-
-// aim:
-// connect to server with ip address 
-// send message to connected server  
-// recieve message from the connected server 
-
-
-
-// open gate ????????
-// how it works ????
+}
