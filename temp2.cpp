87eb1915e89e464d69110903aec859b7cae616a9
BrandonJudd <brandon.judd@outlook.com>
2016-05-27 15:30:49 +1200

    Update temp2.cpp

--- temp2.cpp
+++ temp2.cpp
@@ -94,7 +94,7 @@ int motorControl(double error_signal){
   else if(lose_line()){ //If no line is detected
     set_motor(1,-SPEED);
     set_motor(2,-SPEED);
-    Sleep(0,200000);
+    Sleep(0,050000);
   }
   return 0;
 }
